8a1d6b7d45a8ffc6e8085dceec11af5c54e800d7
Manish Verma <manish.verma@mintworxs.co>
2025-08-17 20:04:37 +0530

    added the console.log to check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  
+
   @Get()
   getHello(): string {
+    console.log('this is the value of the get hello functions')
     return this.appService.getHello();
   }
 }
